945ebe5924e31a82db7427b5622d7cf5409065f9
Anton Makarenko <makarenkoanton2013@gmail.com>
2021-01-28 20:35:44 +0200

    Normal scene loading

--- Assets/Scripts/BossOfficeLoader.cs
+++ Assets/Scripts/BossOfficeLoader.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 public class BossOfficeLoader : MonoBehaviour
 {
-    GirlMovement girl;
+    AudioSource audioSource;
+    [SerializeField] UnityEvent OnSceneLoading;
 
     // Start is called before the first frame update
     void Start()
     {
-        girl = GameObject.FindGameObjectWithTag("Player").GetComponent<GirlMovement>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -19,9 +21,22 @@ public class BossOfficeLoader : MonoBehaviour
         
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
-            SceneManager.LoadScene("BossOffice");
+        {
+            collision.GetComponent<GirlMovement>().enabled = false;
+            collision.GetComponent<Animator>().enabled = false;
+            collision.GetComponent<AudioSource>().enabled = false;
+            OnSceneLoading.Invoke();
+            StartCoroutine(LoadLevel());
+        }
+    }
+
+    IEnumerator LoadLevel()
+    {
+        audioSource.Play();
+        yield return new WaitForSeconds(audioSource.clip.length * 2);
+        SceneManager.LoadScene("BossOffice");
     }
 }
