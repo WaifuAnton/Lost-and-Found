c21ef30c8286dfa5f718413af2135f2d8cda67ae
Anton Makarenko <makarenkoanton2013@gmail.com>
2021-01-28 12:23:24 +0200

    Added chest and some sounds

---
+++ Assets/Scripts/ChestTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestTest : MonoBehaviour
+{
+    AudioSource audioSource;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+        animator.SetTrigger("OnUnlocked");
+        if (!audioSource.isPlaying)
+            audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/GirlMovement.cs
+++ Assets/Scripts/GirlMovement.cs
@@ -6,11 +6,13 @@ public class GirlMovement : MonoBehaviour
 {
     [SerializeField] float speed = 20;
     Rigidbody2D rb2d;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -25,9 +27,17 @@ public class GirlMovement : MonoBehaviour
         Vector2 movement = new Vector2(horizontal, vertical);
         movement *= speed;
         Vector2.ClampMagnitude(movement, speed);
+        //transform.Translate(movement * Time.deltaTime);
         if (movement.magnitude > 0)
+        {
             rb2d.AddForce(movement);
+            if (!audioSource.isPlaying)
+                audioSource.Play();
+        }
         else
+        {
             rb2d.velocity = Vector2.zero;
+            audioSource.Stop();
+        }
     }
 }
