177214f449403d2f239edd9fd911ef32708f48d7
Anton Makarenko <makarenkoanton2013@gmail.com>
2021-01-28 20:04:07 +0200

    Added some colliders and loading

---
+++ Assets/Scripts/BossOfficeLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossOfficeLoader : MonoBehaviour
+{
+    GirlMovement girl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        girl = GameObject.FindGameObjectWithTag("Player").GetComponent<GirlMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            SceneManager.LoadScene("BossOffice");
+    }
+}
