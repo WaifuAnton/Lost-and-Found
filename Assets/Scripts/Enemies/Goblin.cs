3b326cb72ad88b4d0bbf787cc586ff6a89f0eddb
Anton Makarenko <makarenkoanton2013@gmail.com>
2021-01-29 13:03:46 +0200

    Death and idle anim

---
+++ Assets/Scripts/Enemies/Goblin.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goblin : Enemy
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
