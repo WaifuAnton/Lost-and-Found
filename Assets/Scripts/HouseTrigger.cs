ecf2ed5eaa2e75f1763e69eedb0563629c376f26
Anton Makarenko <makarenkoanton2013@gmail.com>
2021-01-29 11:09:26 +0200

    Little better of movement

---
+++ Assets/Scripts/HouseTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Vector3 pos = new Vector3(collision.transform.position.x, collision.transform.position.y, collision.transform.position.z + 6);
+        collision.transform.position = pos;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Vector3 pos = new Vector3(collision.transform.position.x, collision.transform.position.y, collision.transform.position.z - 6);
+        collision.transform.position = pos;
+    }
+}
