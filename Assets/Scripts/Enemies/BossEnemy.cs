2db7a4cbd88be9fda20e718db8339973c2cddb31
Anton Makarenko <makarenkoanton2013@gmail.com>
2021-01-30 04:11:02 +0200

    PAIN

--- Assets/Scripts/Enemies/BossEnemy.cs
+++ Assets/Scripts/Enemies/BossEnemy.cs
@@ -11,28 +11,17 @@ public class BossEnemy : MonoBehaviour
     Animator animator;
     Seeker seeker;
     HittingEnemy hittingEnemy;
-    ThrowingEnemy throwingEnemy;
-    int iteration = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        animator.SetInteger("Iteration", iteration);
         seeker = GetComponent<Seeker>();
         hittingEnemy = GetComponent<HittingEnemy>();
-        throwingEnemy = GetComponent<ThrowingEnemy>();
-        throwingEnemy.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if (hittingEnemy.enabled)
-            HittingLogic();
-    }
-
-    private void HittingLogic()
     {
         if (target == null || Vector2.Distance(transform.position, target.position) >= distance)
         {
@@ -44,10 +33,5 @@ public class BossEnemy : MonoBehaviour
             seeker.enabled = false;
             animator.SetBool("IsAttacking", true);
         }
-    }
-
-    public void NextIteration()
-    {
-        iteration++;
     }
 }
--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -5,12 +5,10 @@ using UnityEngine.Events;
 
 public class Enemy : MonoBehaviour, IDamagable
 {
-    [SerializeField] protected int _health = 1;
+    [SerializeField] protected int health = 1;
     [SerializeField] int contactDamage = 1;
     [SerializeField] UnityEvent OnDead;
     protected Animator animator;
-
-    public int health { get { return _health; } }
 
     private void Awake()
     {
@@ -19,7 +17,7 @@ public class Enemy : MonoBehaviour, IDamagable
 
     protected void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player" && _health > 0)
+        if (collision.gameObject.tag == "Player" && health > 0)
         {
             GirlAttack girl = collision.gameObject.GetComponent<GirlAttack>();
             girl.TakeDamage(contactDamage);
@@ -28,9 +26,9 @@ public class Enemy : MonoBehaviour, IDamagable
 
     public void TakeDamage(int points)
     {
-        _health -= points;
+        health -= points;
         animator.SetTrigger("OnDamaged");
-        if (_health <= 0)
+        if (health <= 0)
             animator.SetTrigger("OnDead");
     }
 
